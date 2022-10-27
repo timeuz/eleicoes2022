c99390882f12cb35992eef9a53d3bf72741ad694
Timeu <timeu.fo@gmail.com>
2022-10-27 18:35:08 -0300

    add votos por estado, melhorias na visualização

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eleicoes2022
-Confere em tempo real a apuração dos votos do segundo turno das eleições presidenciais de 2022
+Aplicação em Python e Flask que confere em tempo real a apuração dos votos do segundo turno das eleições presidenciais de 2022 utilizando dados oficiais do TSE.
+
+## 2DO
+ - Gerar gráficos
+ - Armazenar dados em DB
+ - Otimização para mobile
--- main.py
+++ main.py
@@ -7,17 +7,30 @@ app = Flask(__name__, static_url_path='/projects/eleicoes2022/static')
 @app.route('/')
 
 def votosPresi():
-    data = requests.get(
-        'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json'
+    urna = "545"
+    estados = ("AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PR", "PB", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO")
+    votosest = {'ac':{},'al':{},'ap':{},'am':{},'ba':{},'ce':{},'df':{},'es':{},'go':{},'ma':{},'mt':{},'ms':{},'mg':{},'pa':{},'pr':{},'pb':{},'pe':{},'pi':{},'rj':{},'rn':{},'rs':{},'ro':{},'rr':{},'sc':{},'sp':{},'se':{},'to':{}}
+    aux = 0
+    databr = requests.get(
+        'https://resultados.tse.jus.br/oficial/ele2022/'+urna+'/dados-simplificados/br/br-c0001-e000'+urna+'-r.json'
         )
-    json_data = json.loads(data.content)
+
+    json_databr = json.loads(databr.content)
 
     candidato = []
     partido = []
     votos = []
     porcentagem = []
+    vbrancos = format(int(json_databr['vb']), "1,d").replace(',','.')
+    pvbrancos = json_databr['pvb']+"%"
+    vnulos = format(int(json_databr['tvn']), "1,d").replace(',','.')
+    pvnulos = json_databr['ptvn']+"%"
+    comp = format(int(json_databr['c']), "1,d").replace(',','.')
+    pcomp = json_databr['pc']+"%"
+    abst = format(int(json_databr['a']), "1,d").replace(',','.')
+    pabst = json_databr['pa']+"%"
 
-    for info in json_data['cand']:
+    for info in json_databr['cand']:
         if info['seq'] == '1' or info['seq'] == '2':
             frmt_prcnt = info['pvap']+"%"
             frmt_votos = format(int(info['vap']), "1,d").replace(',','.')
@@ -25,13 +38,41 @@ def votosPresi():
             partido.append(info['cc'])
             votos.append(frmt_votos)
             porcentagem.append(frmt_prcnt)
+    
+    for estado in estados:
+        estado = estado.lower()
+        dataest = requests.get(
+            "https://resultados.tse.jus.br/oficial/ele2022/" + urna + "/dados-simplificados/" + estado + "/" + estado + "-c0001-e000" + urna + "-r.json"
+            )
+        json_dataest = json.loads(dataest.content)
+        for i in json_dataest:
+            if json_dataest['cand'][0]['n'] == '13':
+                votosest[estado]['13'] = json_dataest['cand'][0]['pvap']
+                votosest[estado]['22'] = json_dataest['cand'][1]['pvap']
+            elif json_dataest['cand'][0]['n'] == '22':
+                votosest[estado]['22'] = json_dataest['cand'][0]['pvap']
+                votosest[estado]['13'] = json_dataest['cand'][1]['pvap']
+            else:
+                continue
+    print(votosest)
     return render_template("index.html",
                           len_cand = len(candidato),
                           candidato = candidato,
                           len_vot = len(votos),
                           votos = votos,
                           len_porc = len(porcentagem),
-                          porcentagem = porcentagem
+                          porcentagem = porcentagem,
+                          vbrancos = vbrancos,
+                          pvbrancos = pvbrancos,
+                          vnulos = vnulos,
+                          pvnulos = pvnulos,
+                          comp = comp,
+                          pcomp = pcomp,
+                          abst = abst,
+                          pabst = pabst,
+                          estados = estados,
+                          votosest = votosest,
+                          aux = aux
                           )
 
 if __name__ == '__main__':
